83ad953c04e66ece5c6cc9c56ada4c8ac791a772
Archie <archie.isdiningrat@gmail.com>
2020-02-24 23:47:48 +0700

    provide more robust handler

--- main.py
+++ main.py
@@ -5,14 +5,39 @@ from os import getenv, path
 load_dotenv(path.join(path.dirname(__file__), '.env'))
 app = Flask(__name__)
 
+def getParameter(body, param):
+    if param not in body or body[param] == '':
+        raise ValueError(param + ' is required')
+    return body[param]
+
+def InternalServerError():
+    return jsonify({ 'message': 'something went wrong' }), 500
+
 @app.route('/', methods = ['POST'])
-def text():
-    return jsonify({'message': 'a summary of text', 'data': None })
+def fromText():
+    try:
+        body = request.get_json()
+        text = getParameter(body, 'text')
+
+        return jsonify({'message': 'a summary of text', 'data': text })
+
+    except ValueError as error:
+        return jsonify({ 'message': str(error) }), 422
+    except:
+        return InternalServerError()
 
 @app.route('/link', methods = ['POST'])
-def link():
-    return jsonify({'message': 'a summary of url link', 'data': None })
+def fromLink():
+    try:
+        body = request.get_json()
+        url = getParameter(body, 'url')
 
+        return jsonify({'message': 'a summary of a link', 'data': url })
+
+    except ValueError as error:
+        return jsonify({ 'message': str(error) }), 422
+    except:
+        return InternalServerError()
 
 if __name__ == '__main__':
     app.run(debug=getenv('APP_PORT') == "true", host='0.0.0.0', port=getenv('APP_PORT') or '5001')
