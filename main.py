bd31ba7a4f8b17564d2af75b77becd18daedca87
Archie <archie.isdiningrat@gmail.com>
2020-02-24 13:50:30 +0700

    update code

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 from dotenv import load_dotenv
-from flask import Flask
-from utils import api_response
+from flask import Flask, jsonify, make_response, request
 from os import getenv, path
 
 load_dotenv(path.join(path.dirname(__file__), '.env'))
 app = Flask(__name__)
 
-@app.route('/')
-def handler():
-    return api_response({'message': 'a summary of text', 'data': None })
+@app.route('/', methods = ['POST'])
+def text():
+    return jsonify({'message': 'a summary of text', 'data': None })
 
-@app.route('/link')
-def handler():
-    return api_response({'message': 'a summary of url link', 'data': None })
+@app.route('/link', methods = ['POST'])
+def link():
+    return jsonify({'message': 'a summary of url link', 'data': None })
 
 
 if __name__ == '__main__':
-    app.run(debug=getenv('APP_DEBUG') == "true", host='0.0.0.0', port=getenv('APP_PORT'))
+    app.run(debug=getenv('APP_PORT') == "true", host='0.0.0.0', port=getenv('APP_PORT') or '5001')
--- utils.py
+++
@@ -1,7 +0,0 @@
-from typing import Dict, Any
-from flask import jsonify, make_response
-
-def api_response(payload: Dict[str, Any]) -> Any:
-    return make_response(
-        jsonify(payload)
-    )
