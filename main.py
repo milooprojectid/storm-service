bd6fa5d2697a4b2ef3f8fa8a7adc13318041bd12
tedy_suwega <tedy.suwega@alodokter.com>
2020-02-27 21:44:05 +0700

    add class summarization and summarization

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 from flask import Flask, jsonify, make_response, request
 from os import getenv, path
 from Summarization import summarization
+from NewsLink import getNews
 
 load_dotenv(path.join(path.dirname(__file__), '.env'))
 app = Flask(__name__)
@@ -11,8 +12,12 @@ app = Flask(__name__)
 def text():
     try:
         body = request.get_json()
-        summary = summarization.fit(body['text'])
-        return jsonify({'message': summary, 'data': body['text']})
+        sentence = body['text'] if 'text' in body else ''
+        if sentence == '':
+            return jsonify({'message': 'news is required'})
+
+        summary = summarization.fit(sentence)
+        return jsonify({'message': summary, 'data': sentence})
 
     except:
         return jsonify({'message' : 'something wrong dude'})   
@@ -21,8 +26,18 @@ def text():
 @app.route('/link', methods = ['POST'])
 def link():
 
-    
-    return jsonify({'message': 'a summary of url link', 'data': None })
+    try:
+        body = request.get_json()
+        link = body['text'] if 'text' in body else ''
+        if lnk == '':
+            return jsonify({'message': 'link news is required'})
+
+        sentence = getNews(link)
+        summary = summarization.fit(sentence)
+        return jsonify({'message': summary, 'data': sentence})
+
+    except:
+        return jsonify({'message' : 'something wrong dude'})
 
 
 if __name__ == '__main__':
