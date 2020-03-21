167196ffc60c620ccb6e93cd16cb04462e9f66ea
Archie <archie.isdiningrat@gmail.com>
2020-03-21 20:59:28 +0700

    update deployment

--- client.py
+++ client.py
@@ -1,16 +1,11 @@
 from __future__ import print_function
 import logging
-
 import grpc
-
 import modules.storm_pb2 as storm_pb2
 import modules.storm_pb2_grpc as storm_pb2_grpc
 
 
 def run():
-    # NOTE(gRPC Python Team): .close() is possible on a channel and should be
-    # used in circumstances in which the with statement does not fit the needs
-    # of the code.
     with grpc.insecure_channel('127.0.0.1:6060') as channel:
         stub = storm_pb2_grpc.StormServiceStub(channel)
         response = stub.Summarize(storm_pb2.SummarizeRequest(text='https://kumparan.com/kumparanbisnis/sri-mulyani-tambah-anggaran-corona-rp-62-t-kpk-ancam-hukum-mati-jika-dikorupsi-1t49W501X86'))
